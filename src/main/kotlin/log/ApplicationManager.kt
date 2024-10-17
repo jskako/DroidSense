e57f2fbf43b7c5646662e113bf40496e7f3f4406
Josip Skako <josip.skako@maurer-electronics.hr>
2024-10-17 23:40:43 +0200

    refactoring applicationManager

--- src/main/kotlin/log/ApplicationManager.kt
+++ src/main/kotlin/log/ApplicationManager.kt
@@ -1,8 +1,10 @@
 package log
 
 import adb.ApplicationType
-import androidx.compose.runtime.mutableStateListOf
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.withContext
 
 class ApplicationManager(
@@ -14,9 +16,7 @@ class ApplicationManager(
         identifier: String,
         onDone: (List<AppData>) -> Unit
     ) {
-
         withContext(Dispatchers.IO) {
-
             val applicationTypeIdentifier = when (applicationType) {
                 ApplicationType.SYSTEM -> "s"
                 ApplicationType.USER -> "3"
@@ -34,76 +34,61 @@ class ApplicationManager(
             ).start()
 
             runCatching {
-                val apps = mutableStateListOf<AppData>()
+                val apps = mutableListOf<Deferred<AppData?>>()
+
                 appProcess.inputStream.bufferedReader().useLines { lines ->
                     lines.forEach { packageId ->
                         packageId.replace("package:", "").let {
                             if (it.trim().isNotEmpty()) {
-                                val appPath = getAppPath(
-                                    adbPath = adbPath,
-                                    packageId = it
-                                )
-                                apps.add(
-                                    AppData(
-                                        packageId = it,
-                                        appPath = appPath,
-                                        appSize = getAppSize(
-                                            adbPath = adbPath,
-                                            apkPath = appPath
-                                        ),
-                                        applicationType = applicationType
-                                    )
-                                )
+                                apps.add(async {
+                                    val appPath = getAppPath(adbPath, it)
+                                    val appSize = getAppSize(adbPath, appPath)
+                                    if (appPath != null && appSize != null) {
+                                        AppData(
+                                            packageId = it,
+                                            appPath = appPath,
+                                            appSize = appSize,
+                                            applicationType = applicationType
+                                        )
+                                    } else {
+                                        null
+                                    }
+                                })
                             }
                         }
                     }
                 }
 
-                onDone(apps)
-
-            }.onFailure { exception ->
+                onDone(apps.awaitAll().filterNotNull())
+            }.onFailure {
                 onDone(emptyList())
             }
         }
     }
 
-    private fun clearAppCache(
+    private suspend fun getAppPath(
         adbPath: String,
         packageId: String
-    ) {
-        runCatching {
-            ProcessBuilder(adbPath, "shell", "pm", "clear", packageId).start()
-        }.onFailure {
-
-        }
-    }
-
-    private fun getAppPath(
-        adbPath: String,
-        packageId: String
-    ): String? {
+    ): String? = withContext(Dispatchers.IO) {
         val process = ProcessBuilder(adbPath, "shell", "pm", "path", packageId).start()
 
-        return runCatching {
+        return@withContext runCatching {
             process.inputStream.bufferedReader().useLines { lines ->
                 lines.firstNotNullOfOrNull { line ->
-                    if (line.contains("package:")) {
-                        line.replace("package:", "")
-                    } else null
+                    if (line.contains("package:")) line.replace("package:", "") else null
                 }
             }
         }.getOrNull()
     }
 
-    private fun getAppSize(
+    private suspend fun getAppSize(
         adbPath: String,
         apkPath: String?
-    ): String? {
-        return apkPath?.let {
-
+    ): String? = apkPath?.let {
+        withContext(Dispatchers.IO) {
             val process = ProcessBuilder(adbPath, "shell", "du", "-h", apkPath).start()
 
-            return runCatching {
+            return@withContext runCatching {
                 process.inputStream.bufferedReader().useLines { lines ->
                     lines.firstNotNullOfOrNull { line ->
                         line.split(" ").firstOrNull()?.split("\\s+".toRegex())?.first()
