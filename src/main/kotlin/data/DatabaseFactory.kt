d7db403a1650813cc657b4d026db28004effbfa0
Josip Skako <josip.skako@maurer-electronics.hr>
2024-10-29 10:28:30 +0100

    Update DatabaseFactory.kt

--- src/main/kotlin/data/DatabaseFactory.kt
+++ src/main/kotlin/data/DatabaseFactory.kt
@@ -4,13 +4,19 @@ import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 import com.jskako.DSDatabase
 import kotlin.io.path.Path
-import kotlin.io.path.absolutePathString
+import kotlin.io.path.createDirectories
 import kotlin.io.path.exists
 
 
 fun createDriver(): SqlDriver {
-    val databasePath = Path(System.getProperty("user.home")).resolve(DATABASE_NAME)
-    val driver = JdbcSqliteDriver(url = "jdbc:sqlite:${databasePath.absolutePathString()}")
+    val droidSenseFolderPath = Path(System.getProperty("user.home"), "DroidSense")
+    val databasePath = droidSenseFolderPath.resolve(DATABASE_NAME)
+
+    if (!droidSenseFolderPath.exists()) {
+        droidSenseFolderPath.createDirectories()
+    }
+
+    val driver = JdbcSqliteDriver(url = "jdbc:sqlite:${databasePath.toAbsolutePath()}")
 
     if (!databasePath.exists()) {
         DSDatabase.Schema.create(driver)
