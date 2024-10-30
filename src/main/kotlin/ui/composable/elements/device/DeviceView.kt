b308f2d3fd6585166b7a846a0fce40f8fc017a03
Josip Skako <josip.skako@maurer-electronics.hr>
2024-10-30 13:08:07 +0100

    added vertical scroll to device view

--- src/main/kotlin/ui/composable/elements/device/DeviceView.kt
+++ src/main/kotlin/ui/composable/elements/device/DeviceView.kt
@@ -1,11 +1,19 @@
 package ui.composable.elements.device
 
 import adb.DeviceDetails
+import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import notifications.InfoManagerData
@@ -19,23 +27,42 @@ fun DeviceView(
     onMessage: (InfoManagerData) -> Unit,
     windowStateManager: WindowStateManager
 ) {
-    LazyColumn(
-        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
-        modifier = Modifier.padding(top = 8.dp),
+
+    val listState = rememberLazyListState()
+
+    Box(
+        modifier = Modifier.fillMaxSize()
     ) {
-        items(devices) { device ->
-            DeviceCard(
-                adbPath = adbPath,
-                scrCpyPath = scrCpyPath,
-                device = device,
-                onMessage = onMessage,
-                windowStateManager = windowStateManager,
-                hasMatchingIp = hasMatchingIpForSerialNumber(
-                    devices = devices,
-                    serialNumber = device.serialNumber
+        LazyColumn(
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+            modifier = Modifier.padding(top = 8.dp),
+            state = listState
+        ) {
+            items(devices) { device ->
+                DeviceCard(
+                    adbPath = adbPath,
+                    scrCpyPath = scrCpyPath,
+                    device = device,
+                    onMessage = onMessage,
+                    windowStateManager = windowStateManager,
+                    hasMatchingIp = hasMatchingIpForSerialNumber(
+                        devices = devices,
+                        serialNumber = device.serialNumber
+                    )
                 )
-            )
+            }
         }
+
+        VerticalScrollbar(
+            modifier = Modifier
+                .align(Alignment.CenterEnd)
+                .fillMaxHeight()
+                .padding(end = 5.dp)
+                .width(15.dp),
+            adapter = rememberScrollbarAdapter(
+                scrollState = listState
+            )
+        )
     }
 }
 
