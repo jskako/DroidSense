c619422b4f4adb3c28c6671cd496183545c11ae5
Josip Skako <josip.skako@maurer-electronics.hr>
2024-12-03 01:11:52 +0100

    Update ChatSection.kt

--- src/main/kotlin/ui/composable/sections/ai/ChatSection.kt
+++ src/main/kotlin/ui/composable/sections/ai/ChatSection.kt
@@ -1,15 +1,21 @@
 package ui.composable.sections.ai
 
+import androidx.compose.foundation.LocalScrollbarStyle
+import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -28,6 +34,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import notifications.InfoManagerData
 import ui.composable.elements.DividerColored
@@ -56,6 +64,7 @@ fun ChatSection(
         "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13",
         "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13",
     )
+    var textFieldHeight by remember { mutableStateOf(0) }
 
     LaunchedEffect(searchText) {
         scrollState.animateScrollTo(scrollState.maxValue)
@@ -102,6 +111,7 @@ fun ChatSection(
     }*/
 
     Column(
+        modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
@@ -110,7 +120,7 @@ fun ChatSection(
         ListWithScrollbar(
             modifier = Modifier
                 .weight(0.9f)
-                .fillMaxSize(),
+                .fillMaxWidth(),
             lazyModifier = Modifier.padding(top = 8.dp),
             content = {
                 items(mockedList) {
@@ -123,25 +133,51 @@ fun ChatSection(
         )
 
         Row(
-            modifier = Modifier.background(color = darkBlue),
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = darkBlue),
             verticalAlignment = Alignment.Bottom
         ) {
-
-            TextField(
+            Box(
                 modifier = Modifier
-                    .padding(horizontal = 16.dp, vertical = 8.dp)
                     .weight(1f)
-                    .clip(RoundedCornerShape(16.dp))
-                    .heightIn(max = TextFieldDefaults.MinHeight * 4)
-                    .verticalScroll(scrollState)
-                    .fillMaxWidth()
-                    .background(color = Color.White),
-                value = searchText,
-                onValueChange = { searchText = it },
-                placeholder = { Text(getStringResource("info.search")) },
-                singleLine = false,
-                colors = transparentTextFieldDefault
-            )
+                    .heightIn(
+                        min = TextFieldDefaults.MinHeight,
+                        max = TextFieldDefaults.MinHeight * 4
+                    )
+            ) {
+                TextField(
+                    modifier = Modifier
+                        .padding(horizontal = 20.dp, vertical = 8.dp)
+                        .clip(RoundedCornerShape(16.dp))
+                        .fillMaxWidth()
+                        .onSizeChanged { size ->
+                            textFieldHeight = size.height
+                        }
+                        .verticalScroll(scrollState)
+                        .background(color = Color.White),
+                    value = searchText,
+                    onValueChange = { searchText = it },
+                    placeholder = { Text(getStringResource("info.search")) },
+                    singleLine = false,
+                    colors = transparentTextFieldDefault
+                )
+
+                VerticalScrollbar(
+                    modifier = Modifier
+                        .align(Alignment.CenterEnd)
+                        .height(with(LocalDensity.current) { textFieldHeight.toDp() })
+                        .padding(horizontal = 24.dp, vertical = 8.dp)
+                        .width(6.dp),
+                    adapter = rememberScrollbarAdapter(
+                        scrollState = scrollState
+                    ),
+                    style = LocalScrollbarStyle.current.copy(
+                        hoverColor = darkBlue,
+                        thickness = 6.dp
+                    )
+                )
+            }
 
             TooltipIconButton(
                 modifier = Modifier.padding(end = 8.dp, bottom = 16.dp),
@@ -149,7 +185,7 @@ fun ChatSection(
                 icon = Icons.AutoMirrored.Filled.Send,
                 tooltip = getStringResource("info.edit.name"),
                 function = {
-
+                    // Action here
                 }
             )
         }
